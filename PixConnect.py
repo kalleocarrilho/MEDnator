4dfa07ee342d23d3b0191d5f565dae5e9d1f42aa
kalleo.carrilho <kalleo.carrilho@ebanx.com>
2024-05-17 15:28:54 -0300

    init

---
+++ PixConnect.py
@@ -0,0 +1,15 @@
+import mysqlclient
+
+class PixConnect:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def __enter__(self):
+        self.connection = mysqlclient.connect(host=self.host, user=self.user, password=self.password, database=self.database)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
---
+++ main.py
@@ -0,0 +1,9 @@
+from PixConnect import PixConnect
+
+with PixConnect(host='localhost:3306', user='root', password='abacate1', database='manager') as connection:
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM pix_transaction")
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            print(row)            
