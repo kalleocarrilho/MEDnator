2811b38b2c7d0ef48380f945cd3c33a334fc7091
Raphael Jesus <raphael.andres11@gmail.com>
2024-05-18 09:52:45 -0300

    Fixing API mysql and CORS issues

--- ruleApi.py
+++ ruleApi.py
@@ -3,38 +3,40 @@ from app import app
 from dbConfig import mysql
 from flask import jsonify
 from flask import flash, request
+from flask_cors import cross_origin
 		
 @app.route('/add', methods=['POST'])
 def add_user():
-	try:
-		_json = request.json
-		_first_field = _json['field_1']
-		_second_field = _json['field_2']
-		_operator = _json['operator']
-		_recommendation = _json['recommendation']
-		_reason = _json['reason']
-		_priority = _json['priority']
-		_payee_id = 20
-        
-		if request.method == 'POST':
-			sql = "INSERT INTO rules(first_field, second_field, operator, recommendation, reason, priority, payee_id) VALUES(%s, %s, %s, %s, %s, %s, %s)"
-			data = (_first_field, _second_field, _operator, _recommendation, _reason, _priority, _payee_id)
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute(sql, data)
-			conn.commit()
-			resp = jsonify('Rule added successfully!')
-			resp.status_code = 200
-			return resp
-		else:
-			return not_found()
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+    _json = request.get_json(force=True)
+    _first_field = _json['field_1']
+    _second_field = _json['field_2']
+    _operator = _json['operator']
+    _recommendation = _json['recommendation']
+    _reason = _json['reason']
+    _priority = _json['priority']
+    _payee_id = 20
+
+    if request.method == 'POST':
+        sql = "INSERT INTO rules(first_field, second_field, operator, recommendation, reason, priority, payee_id) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+        data = (_first_field, _second_field, _operator, _recommendation, _reason, _priority, _payee_id)
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            resp = jsonify('Rule added successfully!')
+            resp.status_code = 200
+            return resp
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close() 
+            conn.close()
+    else:
+        return not_found()
 		
 @app.route('/rules')
+@cross_origin()
 def users():
 	try:
 		conn = mysql.connect()
@@ -110,6 +112,5 @@ def not_found(error=None):
     resp = jsonify(message)
     resp.status_code = 404
     return resp
-		
-print(1)
+
 app.run()
